31bce518e38189521b372762abec951aa40204cc
hajer_bouani <hajer.bouani@ensi-uma.tn>
2019-08-14 19:45:34 +0100

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import com.example.demo.Repository.RoleRepository;
+import com.example.demo.model.Role;
+import com.example.demo.model.RoleName;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/Repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.Repository;
+
+import java.util.Optional;
+
+import com.example.demo.model.Role;
+import com.example.demo.model.RoleName;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
